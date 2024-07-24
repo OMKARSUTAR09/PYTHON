1efa8b00fc4ad3a9a27c283e66b5a3b690443dfd
Omkar Sutar <159997804+OMKARSUTAR09@users.noreply.github.com>
2024-07-24 07:56:01 +0530

    Add files via upload
    Assignment operators

---
+++ Assignment operators.py
@@ -0,0 +1,25 @@
+#Assignment operators
+
+num = 10
+num += 5
+print("num  : ",num)
+
+num = 20
+num -= 5
+print("num  : ",num)
+
+num = 30
+num *= 5
+print("num  : ",num)
+
+num = 40
+num /= 5
+print("num  : ",num)
+
+num = 50
+num %= 5
+print("num  : ",num)
+
+num = 60
+num **=5
+print("num  : ",num)
